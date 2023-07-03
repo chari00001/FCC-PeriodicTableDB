c90b2747328c583a3356bba0cfb50def9860c68d
chari00001 <cagrilacin303@gmail.com>
2023-07-03 09:26:37 +0000

    test: test stuff

--- element.sh
+++ element.sh
@@ -4,6 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 if [[ -z $1 ]]
   then
     echo "Please provide an element as an argument."
+    
   else
     re='^[0-9]+$'
     if [[ $1 =~ $re ]]
